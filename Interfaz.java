92dfd473093b7c99083fe590b8156cd5f0e45eef
Luis Pedro Hernandez <lphm0105@gmail.com>
2025-01-30 21:36:16 -0600

    Agregar pre y postcondiciones

--- Interfaz.java
+++ Interfaz.java
@@ -4,6 +4,8 @@ public interface Interfaz {
      * @return
      */
     int evaluar(String operacion); // Evaluar la operación y devolver el resultado
+    // pre: operacion no esta vacía
+    // post: el resultado de evaluar la operación es devuelto o lanza excepción si la expresión es inválida
     /**
      * @param operandoA
      * @param operandoB
@@ -11,4 +13,6 @@ public interface Interfaz {
      * @return
      */
     int operar(int operandoA, int operandoB, String operador); // Realizar operación con dos operandos
+    // pre: operandoA y operandoB son números enteros y operador está en la lista de operadores
+    // post: devuelve el resultado correcto o lanza una excepción si el operador no es válido o si hay división por cero.
 }
